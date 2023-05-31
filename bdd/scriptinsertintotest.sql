1b12329c0cde062ca4ff856ec8980ff80f6a8e8a
EmmieKr <48325116+EmmieKr@users.noreply.github.com>
2023-05-31 07:53:41 +0200

    rectification

--- bdd/scriptinsertintotest.sql
+++ bdd/scriptinsertintotest.sql
@@ -1,4 +1,4 @@
-#QUESTIONNAIRE
+--QUESTIONNAIRE
 
 INSERT INTO Questionnaires (id_questionnaire, tally_id_questionnaire, name_questionnaire, date_creation_questionnaire)
 VALUES (1, 1001, 'Questionnaire 1', '2023-01-01');
@@ -9,18 +9,18 @@ VALUES (2, 1002, 'Questionnaire 2', '2023-02-15');
 INSERT INTO Questionnaires (id_questionnaire, tally_id_questionnaire, name_questionnaire, date_creation_questionnaire)
 VALUES (3, 1003, 'Questionnaire 3', '2023-03-10');
 
-#QUESTION
+--QUESTION
 INSERT INTO Questions (id_question, tally_id_question, label_question, type_question, is_mandatory)
 VALUES (1, 1001, 'Est-ce que vous êtes une femme ou un homme?', 'Choice', TRUE);
 
 INSERT INTO Questions (id_question, label_question, type_question, is_mandatory)
-VALUES (2, 'Est-ce que vous vous sentez bien à l'école?', 'Text', FALSE);
+VALUES (2, 'Est-ce que vous vous sentez bien à lécole?', 'Text', FALSE);
 
 INSERT INTO Questions (id_question, tally_id_question, label_question, type_question)
 VALUES (3, 1002, 'Est-ce que les locaux vous semble agréables?', 'Text');
 
-#USER
-NSERT INTO Users (id_user, tally_id_user, firstname_user, lastname_user, is_admin)
+--USER
+INSERT INTO Users (id_user, tally_id_user, firstname_user, lastname_user, is_admin)
 VALUES (1, 1001, 'Maelys', 'Very', 1);
 
 INSERT INTO Users (id_user, tally_id_user, firstname_user, lastname_user, is_admin)
@@ -41,7 +41,7 @@ VALUES (6, 1006, 'Antoine', 'Aouat', 0);
 INSERT INTO Users (id_user, tally_id_user, firstname_user, lastname_user, is_admin)
 VALUES (7, 1007, 'Thomas', 'Gendron', 0);
 
-#RESPONSES
+--RESPONSES
 INSERT INTO Responses (id_response, tally_id_responses, date_soumission, id_user, id_questionnaire)
 VALUES (1, 2001, '2023-04-01', 1, 1);
 
@@ -58,7 +58,7 @@ INSERT INTO Responses (id_response, tally_id_responses, date_soumission, id_user
 VALUES (5, 2005, '2023-04-10', 2, 1);
 
 
-#HAVE ACCES
+--HAVE ACCES
 INSERT INTO have_access (id_questionnaire, id_user)
 VALUES (1, 1001);
 
@@ -68,24 +68,24 @@ VALUES (2, 1002);
 INSERT INTO have_access (id_questionnaire, id_user)
 VALUES (1, 1003);
 
-#OWN
-INSERT INTO own (id_users, id_questionnaire, id_question)
-VALUES (1, 1, 1);
+--OWN
+INSERT INTO own (id_questionnaire, id_question)
+VALUES (1, 1);
 
-INSERT INTO own (id_users, id_questionnaire, id_question)
-VALUES (2, 1, 2);
+INSERT INTO own (id_questionnaire, id_question)
+VALUES (1, 2);
 
-INSERT INTO own (id_users, id_questionnaire, id_question)
-VALUES (3, 2, 1);
+INSERT INTO own (id_questionnaire, id_question)
+VALUES (2, 1);
 
-INSERT INTO own (id_users, id_questionnaire, id_question)
-VALUES (4, 2, 2);
+INSERT INTO own (id_questionnaire, id_question)
+VALUES (2, 2);
 
-INSERT INTO own (id_users, id_questionnaire, id_question)
-VALUES (5, 3, 3);
+INSERT INTO own (id_questionnaire, id_question)
+VALUES (3, 3);
 
 
-#RESULTS
+--RESULTS
 -- Pour id_response = 1
 INSERT INTO results (id_question, id_response, results)
 VALUES (1, 1, 'femme');
